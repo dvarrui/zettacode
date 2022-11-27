665db47a197ecb79bc1d8a4a1f7c5e05ce6686a1
David Vargas <dvarrui@protonmail.com>
2022-11-27 20:53:12 +0000

    [U] .

--- tasks/devel.rb
+++ tasks/devel.rb
@@ -4,5 +4,6 @@ namespace :devel do
     puts "[INFO] Creating symbolic link into /usr/local/bin"
     basedir = File.join(File.dirname(__FILE__), "..")
     system("sudo ln -s #{basedir}/zettacode /usr/local/bin/zettacode")
+    system("sudo ln -s /usr/local/bin/zettacode /usr/local/bin/zcode")
   end
 end

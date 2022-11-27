d47bf6938b214a7e1148eb0b42dcea497fe5f567
dvarrui <dvarrui@protonmail.com>
2022-11-27 14:55:51 +0000

    [U] .

--- tasks/devel.rb
+++ tasks/devel.rb
@@ -1,7 +1,7 @@
 namespace :devel do
-  desc 'Create launcher'
+  desc "Create launcher"
   task :launcher do
-    puts '[INFO] Creating symbolic link into /usr/local/bin'
+    puts "[INFO] Creating symbolic link into /usr/local/bin"
     basedir = File.join(File.dirname(__FILE__), "..")
     system("sudo ln -s #{basedir}/bin/zettacode /usr/local/bin/zettacode")
   end
